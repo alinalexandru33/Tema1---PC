35265de5d9be53e08b8c253e019d0d24b85f105e
alinalexandru33 <alin.turcitu16@gmail.com>
2022-09-24 21:42:27 +0300

    Task 4 + README Finished

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Tema1---PC
+# Tema1---PC Microprocesor
+
+    The project implements how a processor executes operations (very minimalist).
+
+        Task 1:
+    Decoding the instructions - first 3 bits represent the number of instructions,
+the next 2 * N bits represent N op codes (one op code is formed with 2 bits) and
+the last 4 bits represent the operand dimension.
+
+        Task 2:
+    Executing instructions - using N and Dim we can compute how many numbers 
+(unsigned shorts) we have to read from stdin in order to obtain our operands.
+In this task, Dim can only be a power of 2 (that way every unsigned short will
+always exactly divide in order to get the operands).
+
+        Task 3:
+    Extending the functionality of Task 2 by being able to create operands
+of dimension not power of 2 (the operand can be formed by extracting bits from 2
+or more numbers).
+
+        Task 4:
+    Extending the functionality of Task 2 by respecting the operation order 
+(multiply and divide compute first).
--- task4.c
+++ task4.c
@@ -1,9 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "task.h"
 
-int main() {
+int main(int argc, char const *argv[]) {
+    
+    unsigned int inst, N, dim, operations[10];
+    unsigned short nr, operands[10];
+    int i, j = 0, pos = 3, opNo, idx = 0;  
 
-    int a;
-    scanf("%d", &a);
-    printf("%d", a);
+    scanf("%u", &inst);
+
+    N = extractBitsUint(inst, 3, 0) + 1;
+    dim = extractBitsUint(inst, 4, 3 + 2 * N) + 1;
+    opNo = ((N + 1) * dim) / 16;
+
+    if (((N + 1) * dim) % 16 != 0) {
+        opNo++;
+    }
+    
+    /* Extract the operands */
+    for (i = 0; i < opNo; i++) {
+        scanf("%hd", &nr);
+
+        for (j = 0; j < maxSize; j += dim) {
+            if (pos >= 3 + 2 * N) {
+                break;
+            }
+
+            operands[idx] = extractBitsUShort(nr, dim, j);
+                idx++;
+        }
+    }
+
+    /* Extract the operations */
+    for (i = 0; i < N; i++) {
+        operations[i] = extractBitsUint(inst, 2, pos);
+        pos += 2;
+    }
+
+    
+    /* Execute multiplication and division first */
+    for (i = 0; i < N; i++) {
+        if (operations[i] == 2 || operations[i] == 3) {
+            operands[i] = executeOp(operands[i], operands[i + 1], operations[i]);
+
+            /* Remove one operand */
+            for (j = i + 1; j < idx - 1; j++) {
+                operands[j] = operands[j + 1];
+            }
+            idx--;
+            
+            /* Remove the operations */
+            for (j = i; j < N - 1; j++) {
+                operations[j] = operations[j + 1];
+            }
+            N--;
+            i--;
+        }
+    }
+
+    /* Then execute the rest */
+    for (i = 0; i < N; i++) {
+        operands[i] = executeOp(operands[i], operands[i + 1], operations[i]);
+
+        /* Remove one operand */
+        for (j = i + 1; j < idx - 1; j++) {
+            operands[j] = operands[j + 1];
+        }
+        idx--;
+        
+        /* Remove the operations */
+        for (j = i; j < N - 1; j++) {
+            operations[j] = operations[j + 1];
+        }
+        N--;
+        i--;
+    }
+
+    printf("%d\n", operands[0]);
+
     return 0;
 }
